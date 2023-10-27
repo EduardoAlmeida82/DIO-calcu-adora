2de64a8a15c82e4234b2e15ab6cf0008dccc1161
Eduardo Junior Almeida <edujralmeida@hotmail.com>
2023-10-26 23:02:38 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button `
+    padding: 20px;
+    border: solid ##CDCDCD;
+    background-color:#C0C0C0;
+    color: #4F4F4F;
+    font-size: 24px;
+    font-weight: 700; 
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
